980b6dab7aa2b2753a53c2e7b3015ba9cc085549
yashikavishwakarma <yashika22official@gmail.com>
2026-06-21 15:01:54 +0530

    delpoy

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL =
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 export async function getOptions() {
   const res = await axios.get(`${API_URL}/options`);
